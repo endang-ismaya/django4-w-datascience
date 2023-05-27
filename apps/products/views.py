6c0ec7ce8283462975d2b697e8198e1b5e353f68
Endang Ismaya <endang.ismaya@gmail.com>
2023-05-27 12:17:17 +0700

    add pandas to index for testing

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,6 +1,18 @@
+import pandas as pd
+
 from django.shortcuts import render
+
+from apps.products.models import Product, Purchase
 
 
 def index(request):
-    context = {}
+    products = Product.objects.all()
+    products_pd = pd.DataFrame(products.values())
+
+    purchases = Purchase.objects.all()
+    purchases_pd = pd.DataFrame(purchases.values())
+    context = {
+        "products_pd": products_pd.to_html(),
+        "purchases_pd": purchases_pd.to_html(),
+    }
     return render(request, "products/index.html", context)
