319c3bc175206abb6dfa3834c9dc9dd2157ff557
Endang Ismaya <endang.ismaya@gmail.com>
2023-05-27 17:33:51 +0700

    pd.merge

--- apps/products/views.py
+++ apps/products/views.py
@@ -7,12 +7,23 @@ from apps.products.models import Product, Purchase
 
 def index(request):
     products = Product.objects.all()
-    products_pd = pd.DataFrame(products.values())
+    products_df = pd.DataFrame(products.values())
+    products_df["product_id"] = products_df["id"]
 
     purchases = Purchase.objects.all()
-    purchases_pd = pd.DataFrame(purchases.values())
+    purchases_df = pd.DataFrame(purchases.values())
+
+    # merge
+    df = (
+        pd.merge(purchases_df, products_df, on="product_id")
+        .drop(["id_y", "updated_at_y", "created_at_y"], axis=1)
+        .drop(["updated_at_x"], axis=1)
+        .rename({"id_x": "id", "created_at_x": "created_at"}, axis=1)
+    )
+
     context = {
-        "products_pd": products_pd.to_html(),
-        "purchases_pd": purchases_pd.to_html(),
+        "products_df": products_df.to_html(),
+        "purchases_df": purchases_df.to_html(),
+        "df": df.to_html(),
     }
     return render(request, "products/index.html", context)
