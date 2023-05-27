f2826f8625112512e0cbdcec37bede3f60713406
Endang Ismaya <endang.ismaya@gmail.com>
2023-05-27 11:45:38 +0700

    setup products urls and html template

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 
-from apps.products.models import Product
+from apps.products.models import Product, Purchase
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("name", "created_at", "updated_at")
 
 
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = (
+        "product",
+        "price",
+        "quantity",
+        "total_price",
+        "salesman",
+        "created_at",
+    )
+    exclude = ("total_price",)
+
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Purchase, PurchaseAdmin)
--- apps/products/models.py
+++ apps/products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Product(models.Model):
@@ -8,3 +9,22 @@ class Product(models.Model):
 
     def __str__(self) -> str:
         return str(self.name)
+
+
+class Purchase(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField()
+    total_price = models.PositiveIntegerField(blank=True)
+    salesman = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.total_price = self.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return (
+            f"Solled {self.product.name} - {self.quantity} items for {self.total_price}"
+        )
