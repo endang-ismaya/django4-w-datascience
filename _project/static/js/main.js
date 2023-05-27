b11084709009f1b9673969e6bdb5e6366613e1c5
Endang Ismaya <endang.ismaya@gmail.com>
2023-05-27 21:20:55 +0700

    style error_message

--- _project/static/js/main.js
+++ _project/static/js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
     $(".ui.dropdown").dropdown();
+
+    $(".message .close").on("click", function () {
+        console.log("click it");
+        $(this).closest(".message").transition("fade");
+    });
 });
